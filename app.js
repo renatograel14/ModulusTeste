32a574cd988ae61e6a3073d63839dddc611e19fd
Renato Grael Teixeira <renato.grael@gmail.com>
2015-03-03 11:44:18 -0300

    Correção 'configure'
    Foi retirada a chamada do app.configure()

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 
 //Create a static file server
-app.configure(function() {
-  app.use(express.static(__dirname + '/public'));
-});
+//app.configure(function() {
+//  app.use(express.static(__dirname + '/public'));
+//});
+
+app.use(express.static(__dirname + '/public'));
 
 //Get the dummy data
 require('./server/ddata.js');
